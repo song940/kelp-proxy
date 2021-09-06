002fc3afd799a61f0adf4dd8a5b39d33672094fb
Lsong <song940@gmail.com>
2021-09-06 13:15:01 +0800

    update

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const url   = require('url');
-const http  = require('http');
+const url = require('url');
+const http = require('http');
 const https = require('https');
 
-module.exports = function(config){
-  return function(request, response, next){
-    if(config.path && !~request.url.indexOf(config.path))
+module.exports = function (config) {
+  return function (request, response, next) {
+    if (config.path && request.url.indexOf(config.path) !== 0)
       return next();
-    var options = url.parse(config.proxy, true);
-    options.path = options.path + request.url.replace(config.path, '');
-    var req = (options.protocol == 'http:' ? http : https).request(options, function(res){
+    const remoteURL = config.proxy + request.url.replace(config.path, '');
+    const options = url.parse(remoteURL, true);
+    options.method = request.method;
+    const req = (options.protocol == 'http:' ? http : https).request(options, function (res) {
       response.writeHead(res.statusCode, res.headers);
       res.pipe(response);
     });
-    for(var key in request.headers){
-      if(key != 'host'){
-        req.setHeader(key, request.headers[ key ]);
+    for (var key in request.headers) {
+      if (key != 'host') {
+        req.setHeader(key, request.headers[key]);
       }
     }
     request.pipe(req);
