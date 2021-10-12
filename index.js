a3efc46e8fb3206747ef4bbf2a4957b57b72305d
Lsong <song940@gmail.com>
2021-10-12 18:49:11 +0800

    update

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const url = require('url');
 const http = require('http');
 const https = require('https');
 
-module.exports = function (config) {
+module.exports = config => {
   return function (request, response, next) {
     if (config.path && request.url.indexOf(config.path) !== 0)
       return next();
     const remoteURL = config.proxy + request.url.replace(config.path, '');
     const options = url.parse(remoteURL, true);
     options.method = request.method;
-    const req = (options.protocol == 'http:' ? http : https).request(options, function (res) {
+    const req = (options.protocol == 'http:' ? http : https).request(options, res => {
       response.writeHead(res.statusCode, res.headers);
       res.pipe(response);
     });
-    for (var key in request.headers) {
+    req.on('error', err => console.log('-x proxy', err.message));
+    for (const key in request.headers) {
       if (key != 'host') {
         req.setHeader(key, request.headers[key]);
       }
